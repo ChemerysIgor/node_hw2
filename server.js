3175cc162a6de01dab47177f76909c6ae58383fe
ChemerysIgor <chemeris_igor@ukr.net>
2023-03-08 20:16:10 +0200

    connect database hw 3
    connect database

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
-
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+// 0eIiE49AsMwOxRBe
+const mongoose = require("mongoose");
+const DB_HOST =
+  "mongodb+srv://prosecutor:0eIiE49AsMwOxRBe@cluster0.nsqvl8g.mongodb.net/contacts_reader?retryWrites=true&w=majority";
+mongoose.set(`strictQuery`, true);
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Database connect success");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
